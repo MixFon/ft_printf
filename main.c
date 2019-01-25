3f230ee8a95946ab24896543e46aaa4897277f63
Wispmother Draugr scourge <widraugr@e2r9p2.21-school.ru>
2019-01-25 14:01:07 +0300

    ft_printf

--- main.c
+++ main.c
@@ -1,21 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 13:43:38 by widraugr          #+#    #+#             */
+/*   Updated: 2019/01/25 14:00:52 by widraugr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-void ft_printbit(int n)
+void ft_fullstr(char *str, int n)
+{
+	int		i;
+	char	*ptr;
+
+	i = 0;
+	ptr = ft_itoa(n);
+	while (str[i] != '\0')
+		i++;
+	str[i] = *ptr;
+	free(ptr);
+}
+void ft_printbit(char *str, int n)
 {
 	if (n >= 8)
-		ft_printbit(n / 8);
-	ft_putnbr(n % 8);
+		ft_printbit(str, n / 8);
+	ft_fullstr(str, n % 8);
+//	ft_putnbr(n % 8);
 }
 
 int main()
 {
 	char a;
 	char *b;
+	char *c;
 
-	b = "asdf";
+	//b = "asdf";
 	a = 'E';
-	ft_printbit(20);
-	//ft_printf("Hello\n%0.2oH\n", 1234);
-	printf("Hello\n%#10.5oH\n", 20);
+	b = ft_strnew(12);
+	c = ft_strnew(4);
+	ft_printbit(b, -1);
+	ft_putendl(b);
+	ft_printbit(c, 49);
+	ft_putendl(c);
+	free(b);
+	free(c);
+	//ft_printf("Hello\n%0.5dH\n", 20);
+	printf("Hello\n%oH\n", -3);
 	return 0;
 }

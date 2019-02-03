44632ae3517761ed7d8ad1f7fe69ee2dd516d147
MixFon <mixfon@rambler.ru>
2019-02-03 21:12:49 +0300

    ft_printf

--- main.c
+++ main.c
@@ -11,11 +11,11 @@
 /* ************************************************************************** */
 
 #include "libft_last_ver/ft_printf.h"
-#define LONG_MAX 9223372036854775807
+#define LONG_MAX 2147483647
 #define ULLONG_MAX 18446744073709551615
 int main()
 {
-	int	*i;
+	int	*t;
 //	char	*b;
 //	char	*c;
 //	unsigned int	n;
@@ -40,9 +40,37 @@ int main()
 	*/
 //	str = ft_itoa(n);
 //	ft_putendl(str);
-	ft_printf("%p", 0);
+	/*size_t Gb = 1024*1024;
+	char *a = (char *)malloc(2 * Gb * sizeof(char));
+	char *b = (char *)malloc(2 * Gb * sizeof(char));
+	printf("use %%X: a=%X\n", a);
+	printf("use %%X: b=%X\n", b);
+	printf("use %%p: a=%p\n", a);
+	printf("use %%p: b=%p\n", b); 
+	*/
+/*	int i;
+	printf("int\t%p\n", &i);
+
+	unsigned long l;
+	printf("u long\t%p\n", &l);
+
+	char *str;
+
+	printf("char *\t%p\n", &str);
+
+	printf("strlen\t%p\n", &strlen);
+
+	printf("%p\n", (char*)0);
+*/
+	char  a;
+	char  *b;
+
+	a = 4;
+	b = &a;
+	ft_printf("{%p}",b);
 	ft_putstr("\n");
-	printf("%p", 0);
+	printf("{%p}",b);
+	//printf("\t%llx", LONG_MAX);
 	return 0;
 }
 
